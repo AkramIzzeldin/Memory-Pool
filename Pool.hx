f967d9fc14d56a76daa205343303fa047ea7b7aa
AkramIzzeldin <Akram.Izzeldin@gmail.com>
2014-06-20 19:28:44 +0300

    Update Pool.hx

--- Pool.hx
+++ Pool.hx
@@ -105,28 +105,3 @@ class Copy
 		return null;
 	}
 }
-
-class Main
-{
-	static public function main()
-	{
-		var pool = new Pool(function() { return {x:0, y:0}; } );
-		{
-			var p1 = pool.take();
-			var p2 = pool.take();
-			
-			p1.x = p1.y = 1;
-			p2.x = p2.y = 1;
-			
-			pool.put(p1);
-			pool.put(p2);
-		}
-		var p1 = pool.take(true);
-		var p2 = pool.take();
-		
-		trace(p1);
-		trace(p2);
-		
-		Sys.stdin().readLine();
-	}
-}
