0fd0a4722b9a8b936ce407ebe5e81e801f451327
AkramIzzeldin <Akram.Izzeldin@gmail.com>
2014-06-21 13:59:19 +0300

    Update Pool.hx

--- Pool.hx
+++ Pool.hx
@@ -1,15 +1,13 @@
-import Type;
-
-class Pool<A>
+class Pool<T>
 {
-    public function new (allocator :Void -> A)
+    public function new (allocator:Void -> T)
     {
         _allocator = allocator;
         _newObject = _allocator();
         _freeObjects = [];
     }
     
-    public function take (doInit:Bool = false) :A
+    public function take (doInit:Bool = false) : T
     {
         if (_freeObjects.length > 0) {
             return doInit?init(_freeObjects.pop()):_freeObjects.pop();
@@ -18,90 +16,19 @@ class Pool<A>
         return object;
     }
     
-    public function put (object :A)
+    public function put (object : T)
     {
             _freeObjects.push(object);
     }
     
-    public function init(object :A):A
+    public function init(object:T) : T
     {
-        object = Copy.copy(_newObject);
+        untyped _newObject.copyTo(object);
         return object;
     }
 
-    private var _allocator :Void -> A;
-    private var _freeObjects :Array<A>;
-    private var _newObject:A;
-    private var _fields:Array<String>;
-}
-
-
-class Copy 
-{
-	public static function copy<T>( v:T ) : T  
-	{
-		var vType:ValueType = Type.typeof(v);
-		
-		switch(vType)
-		{
-			case TNull | TInt | TFloat | TBool:
-				return v;
-			case TFunction:
-				return null;
-			case _:
-		}
-		
-		var cType = Type.getClass(v);
-		
-		if (cType != null)
-		{
-			var sType = Type.getClassName(Type.getClass(v));
-			switch(sType)
-			{
-				case "String":
-					return v;
-				case "Array":
-					var result = Type.createInstance(Type.getClass(v), []); 
-					untyped 
-						for ( ii in 0...v.length ) 
-							result.push(copy(v[ii]));
-					return result;
-				case "Map":
-					var result = Type.createInstance(Type.getClass(v), []);
-					untyped 
-					{
-						var keys = v.keys();
-						for ( key in [while (keys.hasNext) keys.next] ) 
-						result.set(key, copy(v.get(key)));
-					} 
-					return result;
-				case "List":
-					var result = Type.createInstance(Type.getClass(v), []);
-					untyped 
-					{
-						var iter : Iterator<Dynamic> = v.iterator();
-						for ( ii in iter ) 
-						{
-							result.add(ii);
-						}
-					} 
-					return result;
-				case _:
-					var obj : Dynamic = {}; 
-					for ( ff in Reflect.fields(v) )
-						Reflect.setField(obj, ff, copy(Reflect.field(v, ff))); 
-					return obj; 
-			}
-		}
-		else
-		{
-			var obj : Dynamic = {}; 
-			for ( ff in Reflect.fields(v) ) 
-			{ 
-				Reflect.setField(obj, ff, copy(Reflect.field(v, ff))); 
-			}
-			return obj;
-		}
-		return null;
-	}
+    private var _allocator : Void -> T;
+    private var _freeObjects : Array<T>;
+    private var _newObject : T;
+    private var _fields : Array<String>;
 }
